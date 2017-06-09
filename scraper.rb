3720b4f96131a090a75c67b850fb4362b6d4ae3e
Daniel O'Connor <daniel.oconnor@gmail.com>
2017-06-09 11:36:43 +0930

    Create scraper.rb
    Fixes #1

--- scraper.rb
+++ scraper.rb
@@ -1,3 +1,3 @@
 require File.join(File.dirname(__FILE__), "planningalerts_xml_feed", "planningalerts_xml_feed")
 
-PlanningAlertsXMLFeed.scrape("http://www.lakemac.com.au/da_submitted.aspx?datespan=past14days")
+PlanningAlertsXMLFeed.scrape("http://www.lakemac.com.au/da_submitted.aspx?datespan=thisweek")
